917ae5f7fa163ef9eab99e88650062d2b4ff470a
Glenn Renfro <grenfro@pivotal.io>
2018-04-27 13:41:38 -0400

    Add List and delete capabilities to CF Scheduler Impl
    resolves #6 #5 #7 #3
    
    Using deployer as a foundation.

---
+++ src/main/java/org/springframework/cloud/scheduler/spi/cloudfoundry/CloudFoundryAppScheduler.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.scheduler.spi.cloudfoundry;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import io.jsonwebtoken.lang.Assert;
+import io.pivotal.scheduler.SchedulerClient;
+import io.pivotal.scheduler.v1.jobs.DeleteJobRequest;
+import io.pivotal.scheduler.v1.jobs.ListJobSchedulesRequest;
+import io.pivotal.scheduler.v1.jobs.ListJobSchedulesResponse;
+import io.pivotal.scheduler.v1.jobs.ListJobsRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.operations.applications.ApplicationSummary;
+import org.cloudfoundry.operations.spaces.SpaceSummary;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.deployer.spi.cloudfoundry.CloudFoundryConnectionProperties;
+import org.springframework.cloud.scheduler.spi.core.ScheduleInfo;
+import org.springframework.cloud.scheduler.spi.core.ScheduleRequest;
+import org.springframework.cloud.scheduler.spi.core.Scheduler;
+import org.springframework.cloud.scheduler.spi.core.SchedulerException;
+import org.springframework.cloud.scheduler.spi.core.SchedulerPropertyKeys;
+
+/**
+ * A Cloud Foundry implementation of the Scheduler interface
+ *
+ * @author Glenn Renfro
+ */
+public class CloudFoundryAppScheduler implements Scheduler {
+
+	private SchedulerClient client;
+
+	private CloudFoundryOperations operations;
+
+	private CloudFoundryConnectionProperties properties;
+
+	protected static final Log logger = LogFactory.getLog(CloudFoundryAppScheduler.class);
+
+	public CloudFoundryAppScheduler(SchedulerClient client, CloudFoundryOperations operations,
+			CloudFoundryConnectionProperties properties) {
+		Assert.notNull(client, "client must not be null");
+		Assert.notNull(operations, "operations must not be null");
+		Assert.notNull(properties, "properties must not be null");
+
+		this.client = client;
+		this.operations = operations;
+		this.properties = properties;
+	}
+
+	@Override
+	public void schedule(ScheduleRequest scheduleRequest) {
+	}
+
+	@Override
+	public void unschedule(String scheduleName) {
+		getJob(scheduleName).flatMap(scheduleJobInfo -> {
+			return this.client.jobs().delete(DeleteJobRequest.builder()
+					.jobId(scheduleJobInfo.getJobId())
+					.build());
+		})
+				.onErrorMap(e -> {
+					if (e instanceof NoSuchElementException) {
+						throw new SchedulerException(String.format("Failed to unschedule, schedule %s does not exist.", scheduleName), e);
+					}
+					throw new SchedulerException("Failed to unschedule: " + scheduleName, e);
+				})
+				.cache()
+				.block();
+	}
+
+	@Override
+	public List<ScheduleInfo> list(String taskDefinitionName) {
+		return getSchedulesList().filter(scheduleInfo ->
+				scheduleInfo.getTaskDefinitionName().equals(taskDefinitionName))
+				.collectList()
+				.cache()
+				.block();
+	}
+
+	@Override
+	public List<ScheduleInfo> list() {
+		return getSchedulesList()
+				.collectList()
+				.cache()
+				.block();
+	}
+
+	private Flux<ScheduleJobInfo> getJobsList() {
+		return this.client.
+				jobs().
+				list(ListJobsRequest.builder()
+						.spaceId(getSpaceId(this.properties.getSpace()))
+						.build()) //Retrieve the ListJobResponse Mono
+				.flatMapIterable(jobs -> jobs.getResources())// iterate over the resources returned.
+				.flatMap(job -> {
+					return getApplication(job.getApplicationId()) // get the application name for each job.
+							.map(optionalApp -> {
+								ScheduleJobInfo scheduleJobInfo = new ScheduleJobInfo();
+								scheduleJobInfo.setScheduleProperties(new HashMap<>());
+								scheduleJobInfo.setScheduleName(job.getName());
+								scheduleJobInfo.setTaskDefinitionName(optionalApp.getName());
+								scheduleJobInfo.setJobId(job.getId());
+								return scheduleJobInfo;
+							});
+				});
+	}
+
+	private Flux<ScheduleInfo> getSchedulesList() {
+		return getJobsList()
+				.flatMap(scheduleJobInfo -> { //iterate over each job and retrieve its schedule expression
+					return getScheduleExpression(scheduleJobInfo.getJobId())
+							.map(listJobSchedulesResponse -> {
+								scheduleJobInfo.getScheduleProperties().put(
+										SchedulerPropertyKeys.CRON_EXPRESSION,
+										listJobSchedulesResponse.getResources().get(0).getExpression());
+								ScheduleInfo scheduleInfo = new ScheduleInfo();
+								scheduleInfo.setTaskDefinitionName(scheduleJobInfo.getTaskDefinitionName());
+								scheduleInfo.setScheduleProperties(scheduleJobInfo.getScheduleProperties());
+								scheduleInfo.setScheduleName(scheduleJobInfo.getScheduleName());
+								return (scheduleInfo);
+							})
+							.onErrorResume(e -> { // if job does not have a schedule associated with it.  Return schedule info with no properties.
+								this.logger.warn(String.format(
+										"%s does not have a schedule expression associated with it.",
+										scheduleJobInfo.getScheduleName()));
+								ScheduleInfo scheduleInfo = new ScheduleInfo();
+								scheduleInfo.setScheduleName(scheduleJobInfo.getScheduleName());
+								scheduleInfo.setTaskDefinitionName(scheduleJobInfo.getTaskDefinitionName());
+								scheduleInfo.setScheduleProperties(scheduleJobInfo.getScheduleProperties());
+								return Mono.just(scheduleInfo);
+							});
+				});
+	}
+
+	public Mono<ScheduleJobInfo> getJob(String scheduleName) {
+		return getJobsList().filter(scheduleInfo ->
+				scheduleInfo.getScheduleName().equals(scheduleName))
+				.single();
+	}
+
+
+	public Mono<ListJobSchedulesResponse> getScheduleExpression(String jobId) {
+		return this.client.jobs()
+				.listSchedules(
+						ListJobSchedulesRequest
+								.builder()
+								.jobId(jobId)
+								.build());
+	}
+
+	private Mono<ApplicationSummary> getApplication(String appId) {
+		return requestListApplications()
+				.filter(application -> appId.equals(application.getId()))
+				.singleOrEmpty();
+	}
+
+	private Flux<ApplicationSummary> requestListApplications() {
+		return this.operations.applications()
+				.list();
+	}
+
+	private String getSpaceId(String spaceName) {
+		return getSpace(spaceName).cache().block().getId();
+	}
+
+	private Mono<SpaceSummary> getSpace(String spaceName) {
+		return requestSpaces()
+				.filter(space -> spaceName.equals(space.getName()))
+				.singleOrEmpty()
+				.cast(SpaceSummary.class);
+	}
+
+	private Flux<SpaceSummary> requestSpaces() {
+		return this.operations.spaces()
+				.list();
+	}
+}
---
+++ src/main/java/org/springframework/cloud/scheduler/spi/cloudfoundry/CloudFoundrySchedulerAutoConfiguration.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.scheduler.spi.cloudfoundry;
+
+import io.pivotal.reactor.scheduler.ReactorSchedulerClient;
+import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.reactor.ConnectionContext;
+import org.cloudfoundry.reactor.TokenProvider;
+import reactor.core.publisher.Mono;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.deployer.spi.cloudfoundry.CloudFoundryConnectionProperties;
+import org.springframework.cloud.scheduler.spi.core.Scheduler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Creates a {@link CloudFoundryAppScheduler}.
+ *
+ * @author Glenn Renfro
+ */
+@Configuration
+@EnableConfigurationProperties
+public class CloudFoundrySchedulerAutoConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean
+	public ReactorSchedulerClient reactorSchedulerClient(ConnectionContext context,
+			TokenProvider passwordGrantTokenProvider,
+			CloudFoundrySchedulerProperties properties) {
+		return ReactorSchedulerClient.builder()
+				.connectionContext(context)
+				.tokenProvider(passwordGrantTokenProvider)
+				.root(Mono.just(properties.getSchedulerUrl()))
+				.build();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public Scheduler scheduler(ReactorSchedulerClient client,
+			CloudFoundryOperations operations,
+			CloudFoundryConnectionProperties properties) {
+		return new CloudFoundryAppScheduler(client, operations, properties);
+	}
+
+
+	@Bean
+	@ConditionalOnMissingBean
+	@ConfigurationProperties(prefix = CloudFoundrySchedulerProperties.CLOUDFOUNDRY_PROPERTIES)
+	public CloudFoundrySchedulerProperties cloudFoundrySchedulerProperties() {
+		return new CloudFoundrySchedulerProperties();
+	}
+
+}
