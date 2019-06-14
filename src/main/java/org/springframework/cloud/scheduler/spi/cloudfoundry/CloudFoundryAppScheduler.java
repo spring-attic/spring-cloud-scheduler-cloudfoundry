ad87b6d5cf4efaa942d15382c8ce0b13d5c6369f
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2019-06-14 21:25:45 +0530

    Pass cmdline args from schedule request
     - Pass cmdline args from ScheduleRequest to AppDeploymentRequest
     - Fix cmdline args test to verify the cmdline argument is passed when scheduling task
    
    Resolves #44
    
    Updated deployer cloud foundry version

--- src/main/java/org/springframework/cloud/scheduler/spi/cloudfoundry/CloudFoundryAppScheduler.java
+++ src/main/java/org/springframework/cloud/scheduler/spi/cloudfoundry/CloudFoundryAppScheduler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -219,7 +219,8 @@ public class CloudFoundryAppScheduler implements Scheduler {
 		AppDeploymentRequest request = new AppDeploymentRequest(
 				scheduleRequest.getDefinition(),
 				scheduleRequest.getResource(),
-				scheduleRequest.getDeploymentProperties());
+				scheduleRequest.getDeploymentProperties(),
+				scheduleRequest.getCommandlineArguments());
 		SummaryApplicationResponse response = taskLauncher.stage(request);
 		return taskLauncher.getCommand(response, request);
 	}
